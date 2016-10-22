a75376a9b6f7db6772f142509ca6953fbaad2276
Dmitry-PC-HOME <gulko.dima@gmail.com>
2016-10-22 21:17:43 +0300

    v2.1

--- JS/slider.js
+++ JS/slider.js
@@ -1,18 +1,18 @@
 var slideWidth=window.innerWidth;
 var sliderTimer;
 $(function(){
-$('.slidewrapper').width($('.slidewrapper').children().size()*slideWidth);
+$('.slidersheets').width($('.slidersheets').children().size()*slideWidth);
     sliderTimer=setInterval(nextSlide,3000);
 });
 
 function nextSlide(){
-    var currentSlide=parseInt($('.slidewrapper').data('current'));
+    var currentSlide=parseInt($('.slidersheets').data('current'));
     currentSlide++;
-    if(currentSlide>=$('.slidewrapper').children().size())
+    if(currentSlide>=$('.slidersheets').children().size())
     {
         currentSlide=0;  
-        $('.slidewrapper').animate({left: -currentSlide*slideWidth},0).data('current',currentSlide);
+        $('.slidersheets').animate({left: -currentSlide*slideWidth},0).data('current',currentSlide);
     }
-    $('.slidewrapper').animate({left: -currentSlide*slideWidth},500).data('current',currentSlide);
+    $('.slidersheets').animate({left: -currentSlide*slideWidth},500).data('current',currentSlide);
 
 }
