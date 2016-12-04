3c6f51210eea95ddedbfbcd8700b0c8e1111841c
dmitry_gulko <gulko.dima@gmail.com>
2016-12-04 20:09:58 +0300

    php(blog), contacts, fixed menu on all pages

---
+++ blog/models/articles.php
@@ -0,0 +1,23 @@
+<?php
+
+    function articles_all($link) {    // Формируем запрос
+        $query = "SELECT * FROM articles ORDER BY id DESC";
+        $result = mysqli_query($link, $query);
+        
+        if(!$result)
+            die(mysqli_error($link));
+        
+    // Извлекаем данные из БД
+        $n = mysqli_num_rows($result);
+        $articles = array();
+        
+        for ($i = 0; $i < $n; $i++)
+        {
+            $row = mysqli_fetch_assoc($result);
+            $articles[] = $row;
+        }
+        
+        return $articles;
+    }
+
+?>
